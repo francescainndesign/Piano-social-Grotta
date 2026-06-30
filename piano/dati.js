81c5b54f558dd7869581aaffd53c08b3ec30b58a
francescainndesign <francescainn.design@gmail.com>
2026-06-30 15:07:46 +0200

    Aggiorna piano (contenuti)

--- piano/dati.js
+++ piano/dati.js
@@ -7,21 +7,11 @@ window.PIANO = {
 
   posts: [
     {
-      data: "2026-07-03", ora: "18:00",
+      data: "", ora: "",
       canale: "Entrambi", formato: "Post",
       media: [
-        { tipo:"foto", src:"media/tartare-grotta-marcello-1.jpg" },
       ],
-      caption: "La sera scende, la roccia resta.\nUn calice, qualcosa da stuzzicare, il tempo che rallenta.\nLuglio comincia così.\n\n",
-      hashtag: ""
-    },
-    {
-      data: "2026-07-11", ora: "",
-      canale: "Entrambi", formato: "Post",
-      media: [
-        { tipo:"foto", src:"media/tartare-grotta-marcello-2.jpg" },
-      ],
-      caption: "Tartare di manzo, salsa tonnata, carasau.\nIl classico che non ti aspetti, raccontato a modo nostro.\nDa assaporare con un calice in mano. ",
+      caption: "",
       hashtag: ""
     }
   ]
